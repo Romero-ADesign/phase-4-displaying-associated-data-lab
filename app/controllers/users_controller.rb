b8003dec5e84dfbc2c133cd8b4c4d2c1b776c5f0
Adam Romero <aromero0904@gmail.com>
2023-02-28 19:12:43 -0500

    commit tree 7c9be25e12ffa31bd3e72abdab8d414eec1ee2be

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+    def index
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user, include: :items
+    end
 end
